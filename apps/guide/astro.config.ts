f2a1f674b2f9cc89e75f44b31d406753d7153df3
async <104009235+kvassani@users.noreply.github.com>
2022-10-21 11:04:45 -0300

    Update astro.config.ts

--- apps/guide/astro.config.ts
+++ apps/guide/astro.config.ts
@@ -14,3 +14,23 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import shikiThemeDarkPlus from 'shiki/themes/dark-plus.json' assert { type: 'json' };
 import Unocss from 'unocss/astro';
+
+const LinkIcon = h(
+  'svg',
+  {
+    width: '1rem',
+    height: '1rem',
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: '2',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+  },
+  h('path', {
+    d: 'M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71',
+  }),
+  h('path', {
+    d: 'M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71',
+  }),
+);
